453cf4f2cdae87c6b3cd20459a0cf61d9f9bed58
Jonathan Peel <me@jonathanpeel.co.za>
2026-04-30 12:26:06 +0300

    Trying support for Safari 9

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { cloudflare } from "@cloudflare/vite-plugin";
 
 export default defineConfig({
   plugins: [legacy({
-    targets: ['safari >= 12'],
+    targets: ['safari >= 9'],
   }), cloudflare()],
   resolve: {
     alias: {
